c0ed07d76e28f6dec84c26cb6e275a9e80293691
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-03-28 14:46:43 +0200

    quest done without dependcies :(

--- src/main/Parse.java
+++ src/main/Parse.java
@@ -11,23 +11,24 @@ public class Parse {
     public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            // write your code here !
+            // write your code here ! okey dokey
 
             // TODO : get the root from the file JSON_WEATHER_PATH
-            JsonNode root = null;
+            //JsonNode root = null;
+            JsonNode root = objectMapper.readTree(new File(JSON_WEATHER_PATH));
 
             // TODO : get the value of "name" attribute
-            String cityName = null;
+            String cityName = root.get("name").asText();
 
             // TODO : get the "lat" and "lon" values of the "coord"
-            Double cityLatitude = null;
-            Double cityLongitude = null;
+            JsonNode coordObject = root.get("coord");
+            Double cityLatitude = coordObject.get("lat").asDouble();
+            Double cityLongitude = coordObject.get("lon").asDouble();
 
             // TODO : get the "wind" attribute as an Wind object
-            Wind wind = null;
-
+            Wind wind = new Wind();
             // TODO : get the "weather" attribute as an array of Weather objects
-            Weather[] weathers = {};
+            Weather[] weathers = objectMapper.convertValue(root.get("weathers"), Weather[].class);
 
             // Don't touch this !
             System.out.printf("City name: %s%n", cityName);
--- tester.sh
+++ tester.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 clear
 javac -cp out:lib/* -d out src/main/*.java
-java -cp out:lib/* Parse
+#//java -cp out:lib/* Parse
